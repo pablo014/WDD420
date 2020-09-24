68c3292f52da7966b14236cd18a3241968b09529
Angelo Pablo <angelopablo014@gmail.com>
2020-09-24 15:11:25 -0600

    added template to contact details

--- week2/cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ week2/cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Contact } from '../contact.model'
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
+  contacts: Contact[] = [
+    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null)
+  ]
   constructor() { }
 
   ngOnInit(): void {
