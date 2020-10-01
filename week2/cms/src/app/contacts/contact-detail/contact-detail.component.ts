58b5a32741329ac5ea53cf85231f0e904999f397
Angelo Pablo <angelopablo014@gmail.com>
2020-10-01 10:02:38 -0600

    added practice stuff

--- week2/cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ week2/cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contact.model'
 @Component({
   selector: 'cms-contact-detail',
@@ -7,9 +7,9 @@ import { Contact } from '../contact.model'
 })
 export class ContactDetailComponent implements OnInit {
 
-  contacts: Contact[] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null)
-  ]
+  contact: Contact = new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null);
+
+
   constructor() { }
 
   ngOnInit(): void {
